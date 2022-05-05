68c5c63877dac2c2e5bf6bc923169879b1745261
avramdj <avramdjordjevic2@gmail.com>
2022-05-06 00:23:11 +0200

    setup update

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-minigrad',
-    version='0.0.1',
-    packages=['minigrad'],
+    packages=find_packages('src'),
     url='https://github.com/avramdj/minigrad',
     license='MIT License',
     author='Avram Djordjevic',
